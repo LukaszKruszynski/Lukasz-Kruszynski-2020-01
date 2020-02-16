e0db3c031667544a8328bda7192c2f2c10707158
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-02-16 20:21:32 +0100

    module 6

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateAdvencedStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateAdvencedStatistics.java
@@ -1,4 +1,44 @@
 package com.kodilla.testing.forum.statistics;
 
-public class CalculateAdvencedStatistics {
+import java.util.List;
+
+public class CalculateAdvencedStatistics implements Statistics {
+    int usersQuantity;
+    int postsQuantity;
+    int commentsQuantity;
+    double averagePostsByUser;
+    double averageCommentsByUser;
+    double averageCommentsByPost;
+
+    public CalculateAdvencedStatistics(int usersQuantity, int postsQuantity, int commentsQuantity) {
+        this.usersQuantity = usersQuantity;
+        this.postsQuantity = postsQuantity;
+        this.commentsQuantity = commentsQuantity;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        double averagePostsByUser = usersQuantity / postsQuantity;
+        double averageCommentsByUser = usersQuantity / commentsQuantity;
+        double averageCommentsByPost = commentsQuantity / postsQuantity;
+
+    }
+
+    public double showStatistics() {
+        return averagePostsByUser + averageCommentsByUser + averageCommentsByPost;
+    }
+
+    @Override
+    public List<String> usersNames() {
+        return null;
+    }
+
+    @Override
+    public int postsCount() {
+        return 0;
+    }
+
+    @Override
+    public int commentsCount() {
+        return 0;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculateAdvencedStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculateAdvencedStatisticsTestSuite.java
@@ -1,4 +1,45 @@
 package com.kodilla.testing.forum.statistics;
 
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class CalculateAdvencedStatisticsTestSuite {
+    @Before
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite: end");
+    }
+
+    @Test
+    public void testCalculateAdvencedStatistics() {
+
+        CalculateAdvencedStatistics calculateAdvencedStatistics1 = new CalculateAdvencedStatistics(2143,6574,73567865);
+        Statistics statisticsMock = mock(Statistics.class);
+        ArrayList<CalculateAdvencedStatistics> statisticsList = new ArrayList<CalculateAdvencedStatistics>();
+        statisticsList.add(calculateAdvencedStatistics1);
+        when(statisticsMock.postsCount()).thenReturn(6574);
+
+    }
 }
+
+
+
