66e25752c96ca2d547ea71b4824c759484bfa3f1
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-02-28 19:30:53 +0100

    8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -7,16 +7,4 @@ public class SecondChallenge {
         }
         return "Done!";
     }
-
-    public static void main(String[] args) {
-        SecondChallenge secondChallenge = new SecondChallenge();
-
-        try {
-            secondChallenge.probablyIWillThrowException(1.5, 1.5);
-        } catch (Exception e) {
-            System.out.println("Your value or values are incorrect. Enter values excepted (x >= 2) and (x < 1) and (y == 1.5)");
-        }finally {
-            System.out.println("Preparing to task...");
-        }
-    }
 }
