5e3bf0cc724fa2cb43c76ea612f86b36818fe76d
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-05-10 18:53:42 +0200

    Pierwsze zadanie Spring. 14.1.

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
