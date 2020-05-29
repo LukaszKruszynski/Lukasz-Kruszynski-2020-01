6a71858bda623a2c2b61c7fdbddeb80768ede640
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-05-29 15:11:56 +0200

    Finish task to commit. 16.6

---
+++ kodilla-free/src/main/java/tests/Tests.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Tests {
+    public static void main(String[] args) {
+        String c = "10";
+        int a = 5;
+        double b = 0.11;
+        System.out.println(c + a + b);
+    }
+}
