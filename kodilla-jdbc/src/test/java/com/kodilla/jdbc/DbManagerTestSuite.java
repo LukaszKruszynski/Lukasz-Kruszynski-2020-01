656898e6e86ffdebfc4354dffe06479a9162d546
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-09-09 15:16:47 +0200

    comment 2 tests

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -18,55 +18,55 @@ public class DbManagerTestSuite {
         Assert.assertNotNull(dbManager.getConnection());
     }
 
-    @Test
-    public void testSelectUsers() throws SQLException {
-        //Given
-        DbManager dbManager = DbManager.getInstance();
-
-        //When
-        String sqlQuery = "SELECT * FROM USERS";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
-
-        //Then
-        int counter = 0;
-        while (rs.next()) {
-            System.out.println(rs.getInt("ID") + ", " +
-                    rs.getString("FIRSTNAME") + ", " +
-                    rs.getString("LASTNAME"));
-            counter++;
-        }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(5, counter);
-    }
-
-    @Test
-    public void testSelectUsersAndPosts() throws SQLException {
-        //Given
-        DbManager dbManager = DbManager.getInstance();
-
-        //When
-        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER\n" +
-                "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID\n" +
-                "GROUP BY P.USER_ID\n" +
-                "HAVING COUNT(*) > 1\n" +
-                "ORDER BY U.LASTNAME, U.FIRSTNAME";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
-
-        //Then
-        int counter = 0;
-        while (rs.next()) {
-            System.out.println(rs.getString("FIRSTNAME")
-                    + " " + rs.getString("LASTNAME")
-                    + ". Number of posts: " + rs.getInt("POSTS_NUMBER"));
-            counter++;
-        }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(2, counter);
-    }
+//    @Test
+//    public void testSelectUsers() throws SQLException {
+//        //Given
+//        DbManager dbManager = DbManager.getInstance();
+//
+//        //When
+//        String sqlQuery = "SELECT * FROM USERS";
+//        Statement statement = dbManager.getConnection().createStatement();
+//        ResultSet rs = statement.executeQuery(sqlQuery);
+//
+//        //Then
+//        int counter = 0;
+//        while (rs.next()) {
+//            System.out.println(rs.getInt("ID") + ", " +
+//                    rs.getString("FIRSTNAME") + ", " +
+//                    rs.getString("LASTNAME"));
+//            counter++;
+//        }
+//        rs.close();
+//        statement.close();
+//        Assert.assertEquals(5, counter);
+//    }
+//
+//    @Test
+//    public void testSelectUsersAndPosts() throws SQLException {
+//        //Given
+//        DbManager dbManager = DbManager.getInstance();
+//
+//        //When
+//        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER\n" +
+//                "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID\n" +
+//                "GROUP BY P.USER_ID\n" +
+//                "HAVING COUNT(*) > 1\n" +
+//                "ORDER BY U.LASTNAME, U.FIRSTNAME";
+//        Statement statement = dbManager.getConnection().createStatement();
+//        ResultSet rs = statement.executeQuery(sqlQuery);
+//
+//        //Then
+//        int counter = 0;
+//        while (rs.next()) {
+//            System.out.println(rs.getString("FIRSTNAME")
+//                    + " " + rs.getString("LASTNAME")
+//                    + ". Number of posts: " + rs.getInt("POSTS_NUMBER"));
+//            counter++;
+//        }
+//        rs.close();
+//        statement.close();
+//        Assert.assertEquals(2, counter);
+//    }
 }
 
 
